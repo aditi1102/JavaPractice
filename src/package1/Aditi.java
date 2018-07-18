641bbd2afbf183e9d1659d6d1941a6421d2df4db
ALOK <ALOK@ALOK-PC>
2018-07-19 00:34:59 +0530

    2nd commit

---
+++ src/package1/Aditi.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class Aditi {
+			
+		public int add(int x , int y)
+		{
+			System.out.println(x+y);
+			return x+y;
+		}
+		
+		public static int sub(int a , int b)
+		{
+			System.out.println(a-b);
+			return a-b;
+		}
+
+	}
+
+
+
---
+++ src/package1/StringHandling.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class StringHandling {
+	
+	public static void  main (String a[])
+	{
+		Aditi ObjA1 = new Aditi();
+		ObjA1.add(5, 0);
+	}
+
+}
